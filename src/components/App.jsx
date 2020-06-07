02d26b18b2e7012c4da47bb268a763a07d563580
natasa <kumricnatasa@gmail.com>
2020-06-07 15:55:14 +0200

    Add Container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import Routes from './Routes';
 import Header from './Header';
 
 const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Routes />
-  </BrowserRouter>
+  <Container>
+    <BrowserRouter>
+      <Header />
+      <Routes />
+    </BrowserRouter>
+  </Container>
 );
 
 export default App;

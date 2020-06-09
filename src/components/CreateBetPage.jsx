1952cbfedac780e3c53f1d7c32b6ff0c94516ba6
natasa <kumricnatasa@gmail.com>
2020-06-09 21:37:39 +0200

    Add form for create bet

--- src/components/CreateBetPage.jsx
+++ src/components/CreateBetPage.jsx
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Input, Button } from 'semantic-ui-react';
+import { Styles } from '../config';
 
-const CreateBetPage = () => (<h2>Create Bet</h2>);
+const CreateBetPage = () => {
+  const [data, setData] = useState({
+    homeTeam: '',
+    awayTeam: '',
+    startTime: '',
+    stake: '',
+  });
+
+  const handleChange = (e, { id, value }) => { setData({ ...data, [id]: value })};
+
+  const handleOnSubmit = () => { };
+
+  return (
+    <Form style={Styles.CREATE_FORM} onSubmit={handleOnSubmit}>
+      <Form.Field
+        id="homeTeam"
+        control={Input}
+        label="Home team"
+        placeholder="Home team"
+        onChange={handleChange}
+      />
+      <Form.Field
+        id="awayTeam"
+        control={Input}
+        label="Away team"
+        placeholder="Away team"
+        onChange={handleChange}
+      />
+      <Form.Field
+        id="startTime"
+        control={Input}
+        label="Start time"
+        placeholder="Start time"
+        onChange={handleChange}
+      />
+      <Form.Field
+        id="stake"
+        control={Input}
+        label="Stake"
+        placeholder="Stake"
+        onChange={handleChange}
+      />
+      <Button style={{ marginLeft: "auto" }} primary type="submit">Create Bet</Button>
+    </Form>
+  );
+}
 
 export default CreateBetPage;
